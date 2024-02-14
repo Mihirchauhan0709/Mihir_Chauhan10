fa32ca37309c9b57dd8b4788b896a83fa32fea5f
Mihir Chauhan <85479391+Mihirchauhan0709@users.noreply.github.com>
2024-02-14 08:59:47 +0000

    Update portfolio description and URL

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -21,8 +21,8 @@ const projectList = [
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Includes my experience, skills and Projects.",
+    url: "https://mihirchauhan.tiiny.site",
   },
   {
     title: "Stock Market Analysis",
